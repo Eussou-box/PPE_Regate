691bd4e2bfc559790da40720265a5e2bd692df36
lstroh <lstroh@S012B-P03.0670086e.ac-strasbourg.fr>
2019-11-15 11:59:41 +0100

    aledRomainenpls

--- PPE_Regate/src/Test/Bateau.java
+++ PPE_Regate/src/Test/Bateau.java
@@ -5,10 +5,8 @@ import java.util.*;
 public class Bateau {
 	private String nomBateau;
 	private String classeBat;
-	private ArrayList<String> lesBateaux;
 	
 	public Bateau() {
-		lesBateaux = new ArrayList<String>();
 		nomBateau = "";
 		classeBat = "";
 	}
@@ -16,10 +14,6 @@ public class Bateau {
 	public Bateau(String leBat, String leType) {
 		nomBateau = leBat;
 		classeBat = leType;
-	}
-	
-	public void ajouteBateaux() {
-		lesBateaux.
 	}
 
 	public String getNomBateau() {
