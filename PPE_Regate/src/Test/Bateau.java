59258a7cfd336b0243052f2d62e4809dc4efe8fd
rmuller <rmuller@S012B-P04.0670086e.ac-strasbourg.fr>
2019-11-15 11:41:36 +0100

    attention

--- PPE_Regate/src/Test/Bateau.java
+++ PPE_Regate/src/Test/Bateau.java
@@ -1,5 +1,42 @@
 package Test;
 
-public class Bateau {
+import java.util.*;
 
+public class Bateau {
+	private String nomBateau;
+	private String classeBat;
+	private ArrayList<String> lesBateaux;
+	
+	public Bateau() {
+		lesBateaux = new ArrayList<String>();
+		nomBateau = "";
+		classeBat = "";
+	}
+	
+	public Bateau(String leBat, String leType) {
+		nomBateau = leBat;
+		classeBat = leType;
+	}
+	
+	public void ajouteBateaux() {
+		lesBateaux.
+	}
+
+	public String getNomBateau() {
+		return nomBateau;
+	}
+
+	public void setNomBateau(String nomBateau) {
+		this.nomBateau = nomBateau;
+	}
+
+	public String getClasseBat() {
+		return classeBat;
+	}
+
+	public void setClasseBat(String classeBat) {
+		this.classeBat = classeBat;
+	}
+	
+	
 }
