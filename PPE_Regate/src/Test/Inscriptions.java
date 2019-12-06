b41449a4fde1efa56438fda63fce52bec81e0c9b
lstroh <lstroh@S012B-P03.0670086e.ac-strasbourg.fr>
2019-12-06 11:13:08 +0100

    Fini Inscriptions

--- PPE_Regate/src/Test/Inscriptions.java
+++ PPE_Regate/src/Test/Inscriptions.java
@@ -1,5 +1,6 @@
 package Test;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.*;
@@ -11,8 +12,28 @@ public class Inscriptions extends JFrame implements ActionListener {
 	private Accueil fenAccueil;
 	
 	public Inscriptions() {
+		this.setTitle("Historique des Régates");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,400);
+		this.setResizable(false);
 		panInscri = new JPanel();
 		btnRetour = new JButton("Retour à l'Accueil");
+		btnRetour.addActionListener(this);
+		panInscri.add(btnRetour);
+		panInscri.add(btnRetour);
+		this.getContentPane().add(panInscri, "Center");
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnRetour) {
+			if(e.getSource() == btnRetour) {
+				fenAccueil = new Accueil();
+				this.dispose();
+				fenAccueil.setVisible(true);
+			}
+		}
 		
 	}
 
