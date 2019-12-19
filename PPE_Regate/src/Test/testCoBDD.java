d9517a0992c6b0cf79add8ed2afce7aa2b00a080
thegh <thegh@192.168.0.27>
2019-12-19 15:47:15 +0100

    ajout classe co bdd wamp 19/12/2019 15:47

---
+++ PPE_Regate/src/Test/testCoBDD.java
@@ -0,0 +1,42 @@
+package Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class testCoBDD {
+	private static final String PILOTE = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/testRegates";
+	private static final String TIMEZONE = "?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+	private static final String LOGIN = "root";
+	private static final String MDP = "";
+	private static Statement st = null;
+	private static Connection cnx = null;
+	private static ResultSet rs = null;
+	
+	public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            cnx = DriverManager.getConnection(URL + TIMEZONE, LOGIN, MDP);
+            st = cnx.createStatement();
+            System.out.println("Connexion a " + URL + " avec succès");
+        } catch (ClassNotFoundException notFoundException) {
+            notFoundException.printStackTrace();
+            System.out.println("Erreur de connecxion");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            System.out.println("Erreur de connecxion");
+        }
+    }
+ 
+    public void close() {
+        try {
+            cnx.close();
+            st.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
